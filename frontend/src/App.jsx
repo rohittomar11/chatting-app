a9022fe5e50adbb1fa55fbc058204a8656ffabf1
Rohit Singh Tomar <rohitsinghtomar1974@gmail.com>
2025-11-24 18:03:08 +0530

    Fix API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Chat from "./pages/Chat";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 function App() {
+  console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
   return (
     <Routes>
 
